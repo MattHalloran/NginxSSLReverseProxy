8899265b9023a0902cadc128fba680ec57cfd6cd
Matthew Halloran <matthalloran8@gmail.com>
2022-07-16 19:40:38 -0400

    change color back
    Text color is changed back after echo

--- scripts/prettify.sh
+++ scripts/prettify.sh
@@ -8,15 +8,15 @@ header() {
 
 # Print info message
 info() {
-    echo "$(tput setaf 3)$1"
+    echo "$(tput setaf 3)$1$(tput setaf 7)"
 }
 
 # Print success message
 success() {
-    echo "$(tput setaf 2)$1"
+    echo "$(tput setaf 2)$1$(tput setaf 7)"
 }
 
 # Print error message
 error() {
-    echo "$(tput setaf 1)$1"
+    echo "$(tput setaf 1)$1$(tput setaf 7)"
 }
